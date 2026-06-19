2c05ad2c55fbbb037c859b92ee67ea9391ed3c7c
Ironman680-0202 <sandeep.chandra52361234@gmail.com>
2026-06-19 14:01:07 +0530

    udate eks

--- eks.tf
+++ eks.tf
@@ -19,7 +19,7 @@ module "eks" {
       min_size       = 1
 
       instance_types = [var.instance_type]
-      ami_type       = "AL2_x86_64"
+      ami_type       = "AL2023_x86_64_STANDARD"
     }
   }
 }
